83703f982fd1b58332195d1e3e241f4229a3b305
Neil Jones <jonesy1567@outlook.com>
2023-07-05 20:47:51 +0100

    ship picture now displays or icon

--- lib/Widgets/shipList.dart
+++ lib/Widgets/shipList.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: file_names, non_constant_identifier_names
-
 import 'package:flutter/material.dart';
 import '../Classes/ships.dart';
+import '../Fonts/custom_icons_icons.dart';
 
 class ShipList extends StatefulWidget {
   final List<Ship>? listItems;
@@ -11,42 +11,58 @@ class ShipList extends StatefulWidget {
 }
 
 class ShipListState extends State<ShipList> {
-  void Like(Function callback) {
-    setState(() {
-      callback();
-    });
+  bool checkString(value) {
+    if (['', null, 0, false].contains(value)) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-              itemCount: widget.listItems!.length,
-              itemBuilder: (context, index) {
-                var post = widget.listItems![index];
-                return Card(
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text(post.name.toString()),
-                          Text(post.shipClass.toString()),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 20.0,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text(post.yearBuilt.toString()),
-                          Text(post.model.toString()),
-                        ],
-                      ),
-                    ],
+      itemCount: widget.listItems!.length,
+      itemBuilder: (context, index) {
+        var ship = widget.listItems![index];
+        var shipImage = ship.image ?? '';
+        return Card(
+          child: Row(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  checkString(ship.image)
+                      ? Image.network(
+                          shipImage,
+                          height: 100,
+                          width: 100,
+                          fit: BoxFit.fill,
+                        )
+                      : const Icon(CustomIcons.ship),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    ship.name.toString(),
+                    textAlign: TextAlign.center,
                   ),
-                );
-              },
-            );
+                  Text(
+                    ship.shipClass.toString(),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
